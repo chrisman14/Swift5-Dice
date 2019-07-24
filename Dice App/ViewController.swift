df749ec2e6d99f6bb08194818efefc6e49947397
Chrisman <chrisman.hyutagalung@gmail.com>
2019-07-24 18:01:24 +0700

    Initial Commit

---
+++ Dice App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by chrisman on 24/07/19.
+//  Copyright © 2019 chrisman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dice AppUITests/Dice_AppUITests.swift
@@ -0,0 +1,41 @@
+//
+//  Dice_AppUITests.swift
+//  Dice AppUITests
+//
+//  Created by chrisman on 24/07/19.
+//  Copyright © 2019 chrisman. All rights reserved.
+//
+
+import XCTest
+
+class Dice_AppUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        // This measures how long it takes to launch your application from the home screen.
+        measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+            XCUIApplication().launch()
+        }
+    }
+}
