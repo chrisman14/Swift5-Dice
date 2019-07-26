20350ba8ce4052b17aa9a87ac37095161226473b
Chrisman <chrisman.hyutagalung@gmail.com>
2019-07-26 13:49:26 +0700

    Firt create app

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var buttonProperti: UIButton!
+    var statusRoll:Bool=false
+    var randomNumberDice1:Int=1
+    var randomNumberDice2:Int=1
+    var timerTest : Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomDice()
         // Do any additional setup after loading the view.
     }
 
+    @objc func randomDice(){
+        randomNumberDice1 = Int.random(in: 1...6)
+        randomNumberDice2 = Int.random(in: 1...6)
+        
+        leftDice.image = UIImage(named: "dice\(randomNumberDice1)")
+        rightDice.image = UIImage(named: "dice\(randomNumberDice2)")
+    }
 
+    @IBAction func rollPress(_ sender: UIButton) {
+        if !statusRoll{
+            buttonProperti.setTitle("Stop", for: .normal)
+            guard timerTest == nil else { return }
+
+              timerTest =  Timer.scheduledTimer(
+                  timeInterval: TimeInterval(0.3),
+                  target      : self,
+                  selector    : #selector(ViewController.randomDice),
+                  userInfo    : nil,
+                  repeats     : true)
+            statusRoll=true
+        
+        }else{
+            buttonProperti.setTitle("Roll", for: .normal)
+            timerTest?.invalidate()
+            timerTest = nil
+            statusRoll=false
+            
+        }
+        
+
+        
+    }
+    
 }
 
